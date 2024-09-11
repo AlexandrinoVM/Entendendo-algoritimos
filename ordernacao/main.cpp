c4ec8593c3de9f8035e60b5a5be30b9d0240fa1e
AlexandrinoVM <vinicius.moraes2@outlook.com>
2024-09-11 15:29:33 -0300

    ordenação por selecão

---
+++ ordernacao/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+int buscarMenor(std::vector<int> &arr){
+        int menor = arr[0];
+        int menor_indice = 0;
+        
+        for(int i=1;i<arr.size();i++){
+            if(arr[i] < menor){
+                menor = arr[i];
+                menor_indice = i;
+            }
+        }
+        return menor_indice;
+}
+
+void ordenarArr(std::vector<int> &arr,int tamanho){
+    int menor = 0;
+    std::vector<int> aux;
+    aux.reserve(tamanho);
+    for(int i=0;i <tamanho;i++){
+        menor = buscarMenor(arr);
+        aux.push_back(arr[menor]);
+        arr.erase(arr.begin() + menor);
+
+        std::cout << aux[i] << std::endl;
+
+    }
+}
+
+
+int main(){
+    std::vector<int> vec={5,2,3,4,1,7};
+
+    ordenarArr(vec,6);
+    return 0;
+}
